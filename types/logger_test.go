22699fd98adedb21c4045575f73e7b7053a5c8d6
YooWaan <rterunuma@gmail.com>
2016-01-01 12:46:53 +0900

    add logger. add rename srvconfig.

---
+++ types/logger_test.go
@@ -0,0 +1,20 @@
+package types
+
+
+import (
+	"testing"
+	"os"
+	"io/ioutil"
+)
+
+func TestInit(t *testing.T) {
+	log := NewAppLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
+	//"fmt"
+	//fmt.Printf("logger %s", log)
+
+	log.Trace.Printf("trace")
+	log.Info.Printf("info")
+	log.Warn.Printf("warn")
+	log.Err.Printf("err")
+}
