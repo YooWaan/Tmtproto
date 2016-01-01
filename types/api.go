e8c5801c454cdba95017e7f8eec6ef7cc330fea5
fracmode <amunu.rets@gmail.com>
2016-01-01 14:59:30 +0900

    [modify] add read/write method on FileSystem
    add read/write method on FileSystem

--- types/api.go
+++ types/api.go
@@ -5,31 +5,28 @@ import (
 	// "os"
 )
 
-type DirectoryEntry interface {
-	io.Closer
+type FsEntry interface {
 	Name() string
 	isDir() bool
 	isFile() bool
 }
 
 type Dir interface {
-	io.Closer
 	Readdir(count int) (map[string]string, error)
 }
 
 type File interface {
-	io.Closer
-	io.ReaderAt
-	io.WriterAt
 	FStat() (map[string]string, error)
 	FSetStat(map[string]string) error
 }
 
 type FileSystem interface {
-	List(name string, flags uint32, attr map[string]string) (DirectoryEntry, error)
+	List(name string, flags uint32, attr map[string]string) (FsEntry, error)
 	Remove(name string) error
 	Rename(old string, new string, flags uint32) error
 	Mkdir(name string, attr map[string]string) error
+	Read(FsEntry) Reader
+	Write(FsEntry) Writer, error
 
 	// TODO: Stat 関連を実装する
 	// Stat(name string, islstat bool) (map[string]string, error)
